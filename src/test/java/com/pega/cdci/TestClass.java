8c21695c187df57932f81811cc152f2a46168ab6
koolharishg <koolharishg@gmail.com>
2020-07-19 23:50:08 +0530

    bazinga commit1

---
+++ src/test/java/com/pega/cdci/TestClass.java
@@ -0,0 +1,23 @@
+package com.pega.cdci;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+	@Test
+	public void test1() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\ghorh\\Documents\\seleniumbazinga\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+		driver.get("https://automationtalks.com/");
+
+	}
+
+}

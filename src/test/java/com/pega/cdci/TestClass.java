b2fef08339899b10c3fffb597638310a9e7770f4
“koolharishg” <koolharishg@gmail.com>
2020-07-20 17:01:23 +0530

    added new test

--- src/test/java/com/pega/cdci/TestClass.java
+++ src/test/java/com/pega/cdci/TestClass.java
@@ -17,7 +17,20 @@ public class TestClass {
 		driver.manage().window().maximize();
 
 		driver.get("https://automationtalks.com/");
-		System.out.println(driver.getTitle());
+		System.out.println("test1----->" + driver.getTitle());
+
+	}
+
+	@Test
+	public void test2() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\ghorh\\Documents\\seleniumbazinga\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+		driver.get("https://automationtalks.com/");
+		System.out.println("test2---->" + driver.getTitle());
 
 	}
 
